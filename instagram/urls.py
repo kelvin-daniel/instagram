3d1b9264b935655c237795d49a3b343558bb8116
kelvin-daniel <kaymutor@gmail.com>
2020-10-16 18:52:29 +0300

    views for index

--- instagram/urls.py
+++ instagram/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('post/', include('post.urls')),
     path('user/', include('authy.urls')),
 ]
 if settings.DEBUG:
--- post/views.py
+++ post/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from post.models import Post, Stream
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+@login_required
+def index(request):
+        user = request.user
+        #gets all string objects created for user
+        posts = Stream.objects.filter(user=user)
+
+        #dict for posts
+        group_ids = []
+        for post in posts:
+            group_ids.append(post.post_id)
+        #filters all ids and order by -post for display in index
+        post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')		
+        
+        return render(request, 'index.html',{'post_items': post_items})
