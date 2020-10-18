a08beb2b4963f81c55bb1155a881bb970efeafba
kelvin-daniel <kaymutor@gmail.com>
2020-10-18 15:08:41 +0300

    add search urls

--- direct/urls.py
+++ direct/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from direct.views import Inbox, Directs,SendDirect
+from direct.views import Inbox, Directs, SendDirect, UserSearch
 
 urlpatterns = [
    	path('', Inbox, name='inbox'),
     path('directs/<username>', Directs, name='directs'),
+    path('new/', UserSearch, name='usersearch'),
     path('send/', SendDirect, name='send_direct'),
 ]
--- direct/views.py
+++ direct/views.py
@@ -57,6 +57,28 @@ def Directs(request, username):
 
 
 @login_required
+def UserSearch(request):
+	query = request.GET.get("q")
+	context = {}
+	
+	if query:
+		users = User.objects.filter(Q(username__icontains=query))
+
+		#Pagination
+		paginator = Paginator(users, 6)
+		page_number = request.GET.get('page')
+		users_paginator = paginator.get_page(page_number)
+
+		context = {
+				'users': users_paginator,
+			}
+	
+	template = loader.get_template('direct/search_user.html')
+	
+	return HttpResponse(template.render(context, request))
+
+
+@login_required
 def SendDirect(request):
 	from_user = request.user
 	to_user_username = request.POST.get('to_user')
@@ -68,4 +90,6 @@ def SendDirect(request):
 		return redirect('inbox')
 	else:
 		HttpResponseBadRequest()
+
+
 
