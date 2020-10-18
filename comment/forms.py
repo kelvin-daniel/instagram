11c4e84ca7153701f2622a6e7519295b9bb307f5
kelvin-daniel <kaymutor@gmail.com>
2020-10-18 13:14:44 +0300

    create feature for DMs

--- comment/forms.py
+++ comment/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from comment.models import Comment
 
 class CommentForm(forms.ModelForm):
-	body = forms.CharField(widget=forms.Textarea(attrs={'class': 'textarea'}), required=True)
+	body = forms.CharField(widget=forms.Textarea(attrs={'class': 'input is-medium'}), required=True)
 
 	class Meta:
 		model = Comment
