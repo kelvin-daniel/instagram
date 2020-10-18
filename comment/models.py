1808fadab4a0cf3f9fb158afeeac722a3db13cfb
kelvin-daniel <kaymutor@gmail.com>
2020-10-18 22:03:00 +0300

    add comments notification

--- comment/models.py
+++ comment/models.py
@@ -12,3 +12,23 @@ class Comment(models.Model):
 	body = models.TextField()
 	date = models.DateTimeField(auto_now_add=True)
 
+	def user_comment_post(sender, instance, *args, **kwargs):
+		comment = instance
+		post = comment.post
+		text_preview = comment.body[:80]
+		sender = comment.user
+		notify = Notification(post=post, sender=sender, user=post.user, text_preview=text_preview ,notification_type=2)
+		notify.save()
+
+	def user_del_comment_post(sender, instance, *args, **kwargs):
+		like = instance
+		post = like.post
+		sender = like.user
+
+		notify = Notification.objects.filter(post=post, sender=sender, notification_type=2)
+		notify.delete()
+
+#Comment
+post_save.connect(Comment.user_comment_post, sender=Comment)
+post_delete.connect(Comment.user_del_comment_post, sender=Comment)
+
