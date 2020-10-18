6107fde42460841f693f0a85db456fac068cf978
kelvin-daniel <kaymutor@gmail.com>
2020-10-18 10:34:06 +0300

    add profile html

--- authy/models.py
+++ authy/models.py
@@ -3,7 +3,20 @@ from django.contrib.auth.models import User
 from post.models import Post
 
 from django.db.models.signals import post_save
+from PIL import Image
+from django.conf import settings
+import os
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    profile_pic_name = 'user_{0}/profile.jpg'.format(instance.user.id)
+    full_path = os.path.join(settings.MEDIA_ROOT, profile_pic_name)
+
+    if os.path.exists(full_path):
+    	os.remove(full_path)
+
+    return profile_pic_name
+	
 # Create your models here.
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
