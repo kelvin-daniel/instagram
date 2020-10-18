6bf5fda269f59abf6a606b0ad53bba156e28fdc7
kelvin-daniel <kaymutor@gmail.com>
2020-10-18 22:34:23 +0300

    register notificationns to admin

--- instagram/settings.py
+++ instagram/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'direct.views.checkDirects',
+                'notifications.views.CountNotifications'
             ],
         },
     },
--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from notifications.views import ShowNotifications
+from notifications.views import ShowNotifications, DeleteNotification
 
 urlpatterns = [
    	path('', ShowNotifications, name='show-notifications'),
-
+   	path('<noti_id>/delete', DeleteNotification, name='delete-notification'),
 ]
